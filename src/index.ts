061969928bbddd86e2bf7110b3a336fcd15242bc
zhuweiyou <i@zhuweiyou.com>
2022-05-29 20:09:05 +0800

    apply: 'build'

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,23 @@
-import { Plugin, ResolvedConfig } from 'vite'
+import { Plugin } from 'vite'
 import { minify, Options } from 'html-minifier-terser'
 
 export function ViteMinifyPlugin(options?: Options): Plugin {
-  let config: ResolvedConfig
-
   return {
     name: 'vite-plugin-minify',
-    configResolved: resolvedConfig => {
-      config = resolvedConfig
-    },
-    transformIndexHtml: {
-      enforce: 'post',
-      transform: html => {
-        if (config.mode !== 'production') {
-          return html
-        }
-
-        return minify(html, {
-          removeComments: true,
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: false,
-          removeEmptyAttributes: true,
-          minifyCSS: true,
-          minifyJS: true,
-          minifyURLs: true,
-          ...options,
-        })
-      },
+    enforce: 'post',
+    apply: 'build',
+    transformIndexHtml: html => {
+      return minify(html, {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: false,
+        removeEmptyAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        ...options,
+      })
     },
   }
 }
