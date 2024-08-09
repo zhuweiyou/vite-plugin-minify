4cdba54b6fcbb5e52a951c8b2adc4cb0d649eac6
Ross Robino <ross@robino.dev>
2024-08-09 10:16:37 -0400

    update deps, add prettier config, jsdoc comment (#5)

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+export default {
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
 import { Plugin } from 'vite'
 import { minify, Options } from 'html-minifier-terser'
 
+/**
+ * @param options [html-minifier-terser options](https://github.com/terser/html-minifier-terser?tab=readme-ov-file#options-quick-reference)
+ *
+ * @default
+ *
+ * {
+ * 	removeComments: true,
+ * 	collapseWhitespace: true,
+ * 	collapseBooleanAttributes: true,
+ * 	removeAttributeQuotes: false,
+ * 	removeEmptyAttributes: true,
+ * 	minifyCSS: true,
+ * 	minifyJS: true,
+ * 	minifyURLs: true,
+ * 	...options, // user provided options
+ * }
+ *
+ * @returns vite-plugin-minify
+ */
 export function ViteMinifyPlugin(options?: Options): Plugin {
   return {
     name: 'vite-plugin-minify',
-    enforce: 'post',
     apply: 'build',
-    transformIndexHtml: (html) => {
-      return minify(html, {
-        removeComments: true,
-        collapseWhitespace: true,
-        collapseBooleanAttributes: true,
-        removeAttributeQuotes: false,
-        removeEmptyAttributes: true,
-        minifyCSS: true,
-        minifyJS: true,
-        minifyURLs: true,
-        ...options,
-      })
+    transformIndexHtml: {
+      order: 'post',
+      handler(html) {
+        return minify(html, {
+          removeComments: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: false,
+          removeEmptyAttributes: true,
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURLs: true,
+          ...options,
+        })
+      },
     },
   }
 }
